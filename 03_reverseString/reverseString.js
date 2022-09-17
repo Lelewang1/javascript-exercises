5e72a9cb97f4518ccd31d63c9fa3e4a82890ec26
Lele <lwangwps@gmail.com>
2022-09-17 16:12:42 +0800

    completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split("");
+    let output = '';
+    for (let i = arr.length - 1; i >= 0; i--) {
+        output += arr[i];
+    }
+    return output; 
 };
+
+//console.log(reverseString('hello world'));
 
 // Do not edit below this line
 module.exports = reverseString;
