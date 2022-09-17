9388431da45ef30c3575de18f648f11c61a4df24
Lele <lwangwps@gmail.com>
2022-09-17 15:49:15 +0800

    completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let output = string;
+    if (number == 1) {
+        return output;
+    }
+    else if (number == 0 || string == '') {
+        return '';
+    }
+    else if (number < 0) {
+        return 'ERROR';
+    }
+    else {
+        for (let i = 1; i < number; i++) {
+            output = output + string;
+        }
+        return output;
+    }
 };
 
 // Do not edit below this line
