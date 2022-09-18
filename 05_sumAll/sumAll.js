4f74375585da4b59c28449285ef57255c6e26dbe
Lele <lwangwps@gmail.com>
2022-09-18 15:20:02 +0800

    completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secNum) {
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secNum) || firstNum < 0 || secNum < 0){
+        return 'ERROR';
+    }
+    let large = 0;
+    let small = 0;
+    let sum = 0;
+    if (firstNum > secNum) {
+        large = firstNum;
+        small = secNum;
+    } 
+    else {
+        large = secNum;
+        small = firstNum
+    }
+    for (let i = small; i <= large; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
