278172599631a737d5cad6858abb05f25b437658
Lele <lwangwps@gmail.com>
2022-09-18 14:43:34 +0800

    completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+    const output = [];
+    console.log(values);
+    for (let i = 0; i < array.length; i++) {
+        if (values.includes(array[i])) {
+            continue;
+        }
+        else {
+            output.push(array[i]);
+        }
+        // output.push(array[i]);
+        // console.log(output);
+    }
+    return output;
 };
+
+const a = [0, 1, 2];
+console.log(removeFromArray(a, 0, 3, 4));
+console.log(a);
 
 // Do not edit below this line
 module.exports = removeFromArray;
