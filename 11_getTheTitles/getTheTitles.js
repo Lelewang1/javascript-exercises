92ac5d5027b5cb5e91ea2f80cc7b08d7c920de5c
Lele <lwangwps@gmail.com>
2022-09-19 20:14:50 +0800

    complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(obj) {
+    let arr = [];
 
+    for (let i = 0; i < obj.length; i++) {
+        arr.push(obj[i]['title']);
+        //console.log(obj[i]['title']);
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
