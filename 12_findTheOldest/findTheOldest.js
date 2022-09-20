c25ba4d0b81cb449f65d709bf7291489209fbb8f
Lele <lwangwps@gmail.com>
2022-09-20 13:34:09 +0800

    completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldest = 0;
+    let age = 0;
+    let output;
+    array.forEach(person => {
+        if (!person.hasOwnProperty('yearOfDeath')) {
+            age = 2022 - person.yearOfBirth;
+            if (age > oldest) {
+                oldest = age;
+                output = person;
+            } 
+        }
+        else {
+            age = person.yearOfDeath - person.yearOfBirth;
+            if (age > oldest) {
+                oldest = age;
+                output = person;
+            } 
+        }
+        
+    });
+    return output;
 };
 
 // Do not edit below this line
